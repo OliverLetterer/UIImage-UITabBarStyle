c24dd311744482d3ac5e1df729d9bda1e4bfdfc9
Oliver Letterer <oliver.letterer@googlemail.com>
2011-09-24 20:44:54 +0200

    added sample application

---
+++ UIImageUITabBarStyleSampleApplication/UIImageUITabBarStyleSampleApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIImageUITabBarStyleSampleApplication
+//
+//  Created by Oliver Letterer on 24.09.11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
